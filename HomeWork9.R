eb06d623f72658fc77267453e8d9366c0670aae9
EconTerrie <Teresa.Mungazi@uvm.edu>
2024-04-24 15:05:30 -0400

    homework

---
+++ HomeWork9.R
@@ -0,0 +1,34 @@
+
+library(tidyverse)
+library(ggplot2)
+
+generate_ID <- function(nSize) {
+  return(1:sum(nSize))
+}
+generate_aware_count <- function(nSize,nMean,nSD) {
+  aware_count <- c(rnorm(n=nSize[1],mean=nMean[1],sd=nSD[1]),
+                   rnorm(n=nSize[2],mean=nMean[2],sd=nSD[2]),
+                   rnorm(n=nSize[3],mean=nMean[3],sd=nSD[3]))H
+  return(aware_count)
+}
+create(ANOdata <- function(ID,TGroup, aware_count)
+  return(ANOdata)
+}
+nGroup <- 3 
+nName <- c("no_policy","hemp_policy","marijuana_policy")
+nSize <- c(35,24,29)
+nMean <- c(8,10,16)
+nSD <- c(5,5,5)
+ID <- generate_ID(nSize)
+aware_count <- generate_aware_count(nSize,nMean,nSD)
+TGroup <- rep(nName,nSize)
+ANOdata <- create_ANOdata(ID,TGroup,aware_count)
+str(ANOdata)
+print(ANOdata)
+ANOmodel <- aov(aware_count ~ TGroup,data=ANOdata)
+print(ANOmoddel)
+print(summary(ANOmodel))
+z <- summary(ANOmodel)
+str(z)
+
+
